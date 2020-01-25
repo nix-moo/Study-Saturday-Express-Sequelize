d8f1aca6297939e1e3cd8f7fef1edd91617e273e
Nikki <nicolette.j.morris@gmail.com>
2020-01-25 11:43:46 -0500

    test module

--- db/models/test.js
+++ db/models/test.js
@@ -3,6 +3,21 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./student');
 
-const Test = db.define();
+const Test = db.define('test', {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  grade: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0,
+      max: 100
+    }
+  },
+});
+
+Test.belongsTo(Student)
 
 module.exports = Test;
