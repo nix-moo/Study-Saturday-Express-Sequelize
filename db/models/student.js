22d89dd36032b4ae953229f3256ac2c8e6e93b79
Nikki <nicolette.j.morris@gmail.com>
2020-01-25 11:11:11 -0500

    student model

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,32 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    }
+  }
+});
+
+const properNouns = (name) => {
+  return name.slice(0,1).toUpperCase() + name.slice(1,);
+}
+Student.beforeCreate((studentInstance, ) => {
+  studentInstance.firstName = properNouns(studentInstance.firstName);
+  studentInstance.lastName = properNouns(studentInstance.lastName);
+
+})
+//Student.sync({force: true});
 
 module.exports = Student;
